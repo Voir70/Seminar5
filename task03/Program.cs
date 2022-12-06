e75ba30ac9e38b8a4894e6b611acc0122c112353
Vladimir Korobov <voir703@gmail.com>
2022-12-06 11:09:18 +0500

    add task 03

---
+++ task03/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] InitArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(-9, 10);
+    }
+
+    return array;
+}
+
+//печатает массив на экран
+void PrintArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}    
+
+    bool IsExist(int[]arr, int number)
+    {
+       for (int i = 0; i < arr.Length; i++)
+       {
+        if(arr[i] == number) return true;
+       }
+       return false; 
+    }
+int[] array = InitArray(5);
+PrintArray(array);
+  Console.WriteLine("Введите число: ");
+  int num = Convert.ToInt32(Console.ReadLine() ?? "");
+   Console.WriteLine(IsExist(array, num));
